bd9fbe0f8c2248e5649302415570209f7d63d632
mrk0011 <marek.jarno.mueller@gmail.com>
2022-11-25 08:51:08 +0100

    changes to code(still not completely working)

--- aocREADMEfiller/src/aocREADMEfiller/Test.java
+++ aocREADMEfiller/src/aocREADMEfiller/Test.java
@@ -1,52 +1,37 @@
 package aocREADMEfiller;
 
-import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		int day = 0;
-		String data = "";
-		String newData = "";
-		
-		Scanner sc = null;
-		Scanner scan1 = null;
-		Scanner scan2 = null;
-		Scanner scan3 = null;
 
-		File Code = new File("test.txt");
-		File Input = new File("testInput.txt");
-		File Output = new File("testOutput.txt");
-		File myFile = new File("READMEtemp.md");
-		
-		try {
-			sc = new Scanner(myFile);
-			scan1 = new Scanner(Code);
-			scan2 = new Scanner(Input);
-			scan3 = new Scanner(Output);
+		Path filePath1 = Path.of("/home/marek/Dokumente/IT/aocREADMEfiller/aocREADMEfiller/READMEtemp.md");
+		String temp = Files.readString(filePath1);
 
-		} catch (FileNotFoundException e) {
-			System.out.println("ERROR! File not found");
-		}
-		data = Files.readString()
-		System.out.println(data);
-		FileWriter README = new FileWriter("README.md");
-		File newFile = new File("README.md");
-		while (scan1.hasNextLine()) {
-			newData = data.replace("CODE", data);
-			
+		Path filePath2 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag01/src/Tag1.java");
+		String code = Files.readString(filePath2);
+		
+		Path filePath3 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag01/input.txt");
+		String input = Files.readString(filePath3);
 
-			
-			
-		}
+		Path filePath4 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag01/output.txt");
+		String output = Files.readString(filePath4);
 		
+		String newtemp;
 		
+		newtemp = temp.replace("CODE", code);
+	
+		temp = newtemp.replace("INPUT", input);
+		newtemp = temp.replace("OUTPUT", output);
 		
+		FileWriter myWriter = new FileWriter(new File("/home/marek/Dokumente/IT/AdventOfCode2022/Tag01/README.md"));
+		myWriter.write(newtemp);
+
 	}
 
 }
