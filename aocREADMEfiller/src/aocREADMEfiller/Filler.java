aacbe511bb5d6db1543ecd55b4bd5376bb8bb10d
mrk0011 <marek.jarno.mueller@gmail.com>
2022-11-25 19:42:49 +0100

    adding of option to select year

--- aocREADMEfiller/src/aocREADMEfiller/Filler.java
+++ aocREADMEfiller/src/aocREADMEfiller/Filler.java
@@ -11,29 +11,33 @@ public class Filler {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int day;
+		int day, year;
 		String newtemp;
+		
+		System.out.println("Welches Jahr möchtest du einlesen?");
+		year = sc.nextInt();
 		
 		System.out.println("Welchen Tag willst du einlesen?");
 		day = sc.nextInt();
 		
+		
 		Path filePath1 = Path.of("/home/marek/Dokumente/IT/aocREADMEfiller/aocREADMEfiller/READMEtemp.md");
 		String temp = Files.readString(filePath1);
 
-		Path filePath2 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag" + String.format("%02d", day) + "/src/Tag" + day + ".java");
+		Path filePath2 = Path.of("/home/marek/Dokumente/IT/AdventOfCode" + year +"/Tag" + String.format("%02d", day) + "/src/Tag" + day + ".java");
 		String code = Files.readString(filePath2);
 
-		Path filePath3 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag" + String.format("%02d", day) + "/input.txt");
+		Path filePath3 = Path.of("/home/marek/Dokumente/IT/AdventOfCode" + year + "/Tag" + String.format("%02d", day) + "/input.txt");
 		String input = Files.readString(filePath3);
 
-		Path filePath4 = Path.of("/home/marek/Dokumente/IT/AdventOfCode2022/Tag" + String.format("%02d", day) + "/output.txt");
+		Path filePath4 = Path.of("/home/marek/Dokumente/IT/AdventOfCode" + year + "/Tag" + String.format("%02d", day) + "/output.txt");
 		String output = Files.readString(filePath4);
 
 		newtemp = temp.replace("CODE", code);
 		temp = newtemp.replace("INPUT", input);
 		newtemp = temp.replace("OUTPUT", output);
 
-		FileWriter myWriter = new FileWriter(new File("/home/marek/Dokumente/IT/AdventOfCode2022/Tag01/README.md"));
+		FileWriter myWriter = new FileWriter(new File("/home/marek/Dokumente/IT/AdventOfCode" + year + "/Tag01/README.md"));
 		myWriter.write(newtemp);
 		
 		myWriter.close();
